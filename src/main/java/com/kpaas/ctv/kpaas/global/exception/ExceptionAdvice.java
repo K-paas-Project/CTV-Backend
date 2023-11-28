21cf59f26e5284b353b9f8897f29beb637d25fca
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-11-28 15:16:00 +0900

    Feat :: 커스텀 예외 추가 Http method Exception
    요청 잘못할 시 방지하는 예외처리 추가

--- src/main/java/com/kpaas/ctv/kpaas/global/exception/ExceptionAdvice.java
+++ src/main/java/com/kpaas/ctv/kpaas/global/exception/ExceptionAdvice.java
@@ -4,8 +4,12 @@ import com.kpaas.ctv.kpaas.global.common.dto.BaseResponse;
 import lombok.Builder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Arrays;
+import java.util.Objects;
 
 @RestControllerAdvice
 public class ExceptionAdvice {
@@ -19,5 +23,13 @@ public class ExceptionAdvice {
                 .message(ex.getError().getMessage())
                 .build();
         return new ResponseEntity<>(response, ex.getError().getStatus());
+    }
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<BaseResponse> handleMethodNotSupported(HttpRequestMethodNotSupportedException ex) {
+        BaseResponse baseResponse = new BaseResponse();
+        baseResponse.of(HttpStatus.METHOD_NOT_ALLOWED, "요청 메서드 잘못됨 " + Arrays.toString(Objects.requireNonNull(ex.getSupportedMethods())) + " 메서드로 요청 부탁");
+
+
+        return new ResponseEntity<>(baseResponse, HttpStatus.METHOD_NOT_ALLOWED);
     }
 }
