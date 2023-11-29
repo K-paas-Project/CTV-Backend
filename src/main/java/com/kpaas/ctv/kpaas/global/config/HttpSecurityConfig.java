3f4678da9577adbb12409f0f7fd8d6e1e0fd54c1
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-11-29 15:54:34 +0900

    Feat :: Swagger 권한 설정 (허용)

--- src/main/java/com/kpaas/ctv/kpaas/global/config/HttpSecurityConfig.java
+++ src/main/java/com/kpaas/ctv/kpaas/global/config/HttpSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -31,6 +32,7 @@ public class HttpSecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/api/v1/refresh").permitAll()
                         .requestMatchers("/api/v1/join", "/api/v1/login").permitAll()
+                        .requestMatchers("/swagger-ui/*").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sessionManagement ->
@@ -38,5 +40,14 @@ public class HttpSecurityConfig {
                 )
                 .addFilterBefore(new JwtTokenFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class);
         return  httpSecurity.build();
+    }
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers("/h2-console/**",
+                "/favicon.ico",
+                "/error",
+                "/swagger-ui/**",
+                "/swagger-resources/**",
+                "/v3/api-docs/**");
     }
 }
