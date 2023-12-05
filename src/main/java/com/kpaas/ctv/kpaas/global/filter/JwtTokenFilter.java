7a3332448d966c036a24a7d7d8a85c4502b1c34a
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-12-05 15:41:04 +0900

    Fix :: TokenFiler 리턴 포함하여 기존 값 리턴 안되던 문제 해결

--- src/main/java/com/kpaas/ctv/kpaas/global/filter/JwtTokenFilter.java
+++ src/main/java/com/kpaas/ctv/kpaas/global/filter/JwtTokenFilter.java
@@ -47,7 +47,7 @@ public class JwtTokenFilter extends OncePerRequestFilter {
                 authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-                return;
+
             } catch (JwtException jwtException) {
                 response.sendError(HttpServletResponse.SC_UNAUTHORIZED, jwtException.getMessage());
                 return;
